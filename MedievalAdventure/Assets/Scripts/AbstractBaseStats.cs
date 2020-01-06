55862223eeaa7bac7882ed304737f0f0efb044aa
Muzard Geoffrey <shadowhite@hotmail.fr>
2020-01-06 14:02:40 +0100

    Create AbstractBaseStats

---
+++ MedievalAdventure/Assets/Scripts/AbstractBaseStats.cs
@@ -0,0 +1,26 @@
+﻿/* -------------------------------------------------------------- */
+/* -----------All rights reserved to Medieval Adventure---------- */
+/* -------------------------------------------------------------- */
+
+using UnityEngine;
+
+public abstract class AbstractBaseStats : MonoBehaviour
+{
+    protected int ActionPoints;
+
+    protected int CriticalRate;
+
+    protected int LifePoint;
+
+    protected int MagicalDamage;
+
+    protected int MagicalPenetration;
+
+    protected int MagicalResist;
+
+    protected int PhysicalDamage;
+
+    protected int PhysicalPenetration;
+
+    protected int PhysicalResist;
+}
