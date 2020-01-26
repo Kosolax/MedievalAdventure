0ee6eec7db3185d1d460d9735f52f735fb80ff24
Shadowhite <shadowhite@hotmail.fr>
2020-01-26 16:38:29 +0100

    Classe mere BaseStats pour les heros et les mobs

---
+++ MedievalAdventure/Assets/Scripts/BaseStats.cs
@@ -0,0 +1,88 @@
+﻿/* -------------------------------------------------------------- */
+/* -----------All rights reserved to Medieval Adventure---------- */
+/* -------------------------------------------------------------- */
+
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+    /*
+    ActionPoints.Description = "Permet de faire des actions et de bouger";
+    ActionPoints.Title = "Point d'Action";
+    ActionPoints.Icone = null;
+    ActionPoints.Value = 0;
+    */
+    public Stats ActionPoints;
+
+    /*
+    CriticalRates.Description = "Permet de realiser des coups critiques";
+    CriticalRates.Title = "Taux de Critique";
+    CriticalRates.Icone = null;
+    CriticalRates.Value = 0;
+    */
+    public Stats CriticalRates;
+
+    /*
+    Heal.Description = "Points de vie";
+    Heal.Title = "Vie";
+    Heal.Icone = null;
+    Heal.Value = 0;
+    */
+    public Stats Heal;
+
+    /*
+    MagicalDamages.Description = "Puissance magique";
+    MagicalDamages.Title = "Dommage Magique";
+    MagicalDamages.Icone = null;
+    MagicalDamages.Value = 0;
+    */
+    public Stats MagicalDamages;
+
+    /*
+    MagicalPenetrations.Description = "Permet de d'ignorer la resistance magique";
+    MagicalPenetrations.Title = "Penetration Magique";
+    MagicalPenetrations.Icone = null;
+    MagicalPenetrations.Value = 0;
+    */
+    public Stats MagicalPenetrations;
+
+    /*
+    MagicalResistances.Description = "Permet de recevoir moins de degats magiques";
+    MagicalResistances.Title = "Resistance Magique";
+    MagicalResistances.Icone = null;
+    MagicalResistances.Value = 0;
+    */
+    public Stats MagicalResistances;
+
+    /*
+    PhysicalDamages.Description = "Degat Physique";
+    PhysicalDamages.Title = "Dommage Physique";
+    PhysicalDamages.Icone = null;
+    PhysicalDamages.Value = 0;
+    */
+    public Stats PhysicalDamages;
+
+    /*
+    PhysicalPenetrations.Description = "Permet d'ignorer la resistance physique";
+    PhysicalPenetrations.Title = "Penetration Physique";
+    PhysicalPenetrations.Icone = null;
+    PhysicalPenetrations.Value = 0;
+    */
+    public Stats PhysicalPenetrations;
+
+    /*
+    PhysicalResistances.Description = "Permet de recevoir moins de degats physiques";
+    PhysicalResistances.Title = "Resistance Physique";
+    PhysicalResistances.Icone = null;
+    PhysicalResistances.Value = 0;
+    */
+    public Stats PhysicalResistances;
+
+    /*
+    Speed.Description = "Permet de derminer l'ordre des tours";
+    Speed.Title = "Vitesse";
+    Speed.Icone = null;
+    Speed.Value = 0;
+    */
+    public Stats Speed;
+}
