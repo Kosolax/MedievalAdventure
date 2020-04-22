5acd7d3bdb2f7302705f096d784860e667c33af2
JaviFdezC <46001349+JaviFdezC@users.noreply.github.com>
2020-04-22 14:37:38 +0200

    Pause menu uploaded

---
+++ MedievalAdventure/Assets/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  
+  public int speed = 5;
+  private Vector3 DirectionDeplacement = Vector3.zero;
+  private CharacterController Player;
+  public int Sensibility;
+  public int Jump = 5;
+  public int Gravity = 20;
+ 
+  void Start()
+  {
+    Player = GetComponent<CharacterController>();
+  }
+  // Update is called once per frame
+  void Update () 
+  {
+    DirectionDeplacement.z = Input.GetAxis("Vertical");
+    DirectionDeplacement.x = Input.GetAxis("Horizontal");
+    DirectionDeplacement = transform.TransformDirection(DirectionDeplacement);
+    
+    // Déplacement
+    Player.Move(DirectionDeplacement * Time.deltaTime * speed);
+    transform.Rotate(0, Input.GetAxisRaw("Mouse X")*Sensibility, 0);
+
+    // Saut
+    if(Input.GetKeyDown(KeyCode.Space) && Player.isGrounded)
+    {
+     DirectionDeplacement.y = Jump;
+	}
+
+    // Gravité
+    if(!Player.isGrounded)
+    {
+     DirectionDeplacement.y -= Gravity * Time.deltaTime;
+	}
+  }
+}
---
+++ MedievalAdventure/Assets/Scripts/PauseMenuV2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuV2 : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You left the game");
+        Application.Quit();
+    }
+    
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
