1f5f3e6e3af99d419f449a3cd160a11871c38ec3
Combemorel <combemorel_dorian@orange.fr>
2020-04-23 14:47:46 +0200

    ajout fonction manuel si loadscene instant

--- MedievalAdventure/Assets/Scripts/LevelLoader.cs
+++ MedievalAdventure/Assets/Scripts/LevelLoader.cs
@@ -18,21 +18,56 @@ public class LevelLoader : MonoBehaviour
 
     IEnumerator LoadsAsync(int sceneIndex)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        float timer = 0f;
+        float minLoadTime = 2.5f;
+        float slideValue = 0f;
+        float progressValue = 0f;
+        
 
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
         loadingScreen.SetActive(true);
 
-        while (!operation.isDone)
+        while (!operation.isDone || timer < minLoadTime)
         {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
 
-            slider.value = progress;
+            timer += Time.deltaTime;
+            if(timer < minLoadTime || operation.isDone)
+            {
+                if (timer > 0.625 && timer < 0.700)
+                {
+                    slideValue = 0.25f;
+                    progressValue = 25f;
+                }
+                else if (timer > 1.250 && timer < 1.300)
+                {
+                    slideValue = 0.50f;
+                    progressValue = 50f;
+                }
+                else if (timer > 1.875 && timer < 1.900)
+                {
+                    slideValue = 0.75f;
+                    progressValue = 75f;
+                }
+                else if (timer > 2.300 && timer < 2.400)
+                {
+                    slideValue = 1f;
+                    progressValue = 100;
+                }
 
-            progressText.text = progress * 100 + "%";
-
+                slider.value = slideValue;
+                progressText.text = progressValue + "%";
+            }
+            else if (!operation.isDone)
+            {
+                slider.value = progress;
+                progressText.text = progress * 100 + "%";
+                operation.allowSceneActivation = true;
+            }
             yield return null;
-
-
         }
+        yield return null;
     }
 }
